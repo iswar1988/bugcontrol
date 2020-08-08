3c2330a124669fee96823480688a5e395506a752
iswar <iswar_bencosme@hotmail.com>
2020-08-07 20:27:45 -0400

    First Commit

---
+++ src/main/java/com/dev/bugcontrol/BugcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.dev.bugcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/bugcontrol/BugcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.bugcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
